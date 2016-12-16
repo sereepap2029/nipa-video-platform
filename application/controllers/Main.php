aee8d00bf8b4709f72943f35d2dee0193208e7e3
sereepap2029@gmail.com <1449900239>
2016-12-16 15:07:11 +0700

    init codeigniter

---
+++ application/controllers/Main.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	public function __construct() {
+        parent::__construct();
+        
+    }
+	public function index()
+	{
+		$this->load->view('welcome_message');
+		$this->load->view('welcome_message');
+	}
+	public function index2()
+	{
+		$this->load->view('welcome_message');
+	}
+}
