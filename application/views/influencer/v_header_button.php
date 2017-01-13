a797d89d1cb327e4ba6ee3fb780956649be1a9ed
sereepap <sereepap2029@gmail.com>
2017-01-13 09:09:28 +0700

    update link button

--- application/views/influencer/v_header_button.php
+++ application/views/influencer/v_header_button.php
@@ -1,10 +1,10 @@
 <div class="row">
     <div class="small-10 columns">
-        <a class="button primary" href="#">Dashboard</a>
-        <a class="button primary" href="#">Sponsorship</a>
-        <a class="button primary" href="#">Partnership</a>
-        <a class="button primary" href="#">My Job</a>
-        <a class="button primary" href="#">Chat Room</a>
-        <a class="button primary" href="#">Reward</a>
+        <a class="button primary" href="<?=site_url("influencer/dashboard")?>">Dashboard</a>
+        <a class="button primary" href="<?=site_url("influencer/sponsorship")?>">Sponsorship</a>
+        <a class="button primary" href="<?=site_url("influencer/partnership")?>">Partnership</a>
+        <a class="button primary" href="<?=site_url("influencer/myjob")?>">My Job</a>
+        <a class="button primary" href="<?=site_url("influencer/chatroom")?>">Chat Room</a>
+        <a class="button primary" href="<?=site_url("influencer/reward")?>">Reward</a>
     </div>
 </div>
--- application/views/producer/v_header_button.php
+++ application/views/producer/v_header_button.php
@@ -1,10 +1,10 @@
 <div class="row">
     <div class="small-10 columns">
-        <a class="button primary" href="#">Dashboard</a>
-        <a class="button primary" href="#">Sponsorship</a>
-        <a class="button primary" href="#">Partnership</a>
-        <a class="button primary" href="#">My Job</a>
-        <a class="button primary" href="#">Chat Room</a>
-        <a class="button primary" href="#">Reward</a>
+        <a class="button primary" href="<?=site_url("producer/dashboard")?>">Dashboard</a>
+        <a class="button primary" href="<?=site_url("producer/sponsorship")?>">Sponsorship</a>
+        <a class="button primary" href="<?=site_url("producer/partnership")?>">Partnership</a>
+        <a class="button primary" href="<?=site_url("producer/myjob")?>">My Job</a>
+        <a class="button primary" href="<?=site_url("producer/chatroom")?>">Chat Room</a>
+        <a class="button primary" href="<?=site_url("producer/reward")?>">Reward</a>
     </div>
 </div>
