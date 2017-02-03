c278bd2bb2f69b083807393861e1c8d376679d96
sereepap <sereepap2029@gmail.com>
2017-02-03 15:49:35 +0700

    fabric progress WIP campaign

--- application/controllers/ajax/Campaign.php
+++ application/controllers/ajax/Campaign.php
@@ -10,6 +10,28 @@ class Campaign extends CI_Controller {
         $this->load->model('m_influencer');
         $this->load->model('m_campaign');
         $this->load->model('m_time');
+        $auth=false;
+        if ($this->m_session_cache->get('brand_id')) {
+            $user_data = $this->m_brand->get_brand_by_id($this->m_session_cache->get('brand_id'));
+            if (isset($user_data->username)) {
+            	$auth=true;
+            }
+        }
+        if ($this->m_session_cache->get('influencer_id')) {
+            $user_data = $this->m_influencer->get_influencer_by_id($this->m_session_cache->get('influencer_id'));
+            if (isset($user_data->username)) {
+            	$auth=true;
+            }
+        }
+        if ($this->m_session_cache->get('producer_id')) {
+            $user_data = $this->m_producer->get_producer_by_id($this->m_session_cache->get('producer_id'));
+            if (isset($user_data->username)) {
+            	$auth=true;
+            }
+        }
+        if (!$auth) {
+        	redirect('login/logout');
+        }
     }
 	// ajax handle region
 	public function get_camp_detail()
@@ -20,6 +42,57 @@ class Campaign extends CI_Controller {
 		$data['creators_invited']=$this->m_campaign->get_campaign_has_creator_by_campaign_id($campaign_id,"invite");
 		$data['creators_submit']=$this->m_campaign->get_campaign_has_creator_by_campaign_id($campaign_id,"submit");
 		$this->load->view('ajax/campaign/v_creator_campaign_detail',$data);
+		
+	}
+	public function update_camp_detail()
+	{
+		header('Content-Type: application/json');
+        $json = array();
+        $json['flag']="OK";
+        $campaign_id=$_POST['camp_id'];
+			    $data_so = array(
+					'name' => $_POST['name'], 
+					'description' => $_POST['description'],
+					'url' => $_POST['url'],
+					'start_date' => $this->m_time->datepicker_to_unix($_POST['start_date']),
+					'end_date' => $this->m_time->datepicker_to_unix($_POST['end_date']),
+					);
+				$this->m_campaign->update_campaign($data_so,$campaign_id);
+        
+        echo json_encode($json);
+		
+	}
+	public function update_camp_detail_cre()
+	{
+		header('Content-Type: application/json');
+        $json = array();
+        $json['flag']="OK";
+        $campaign_id=$_POST['camp_id'];
+        $old_campaign=$this->m_campaign->get_campaign_by_id($campaign_id);
+        $data_so = array(
+			'progress' => $_POST['progress'], 
+		);
+        if ($_POST['file']!="") {
+        	@unlink("./media/campaign/submit/" . $old_campaign->file);
+	        $new_filename=$campaign_id."_".time();
+	        $result1=$this->m_campaign->add_campaign_file($_POST['file'],$campaign_id,$new_filename,'submit');
+	        
+	        if ($result1 === FALSE) { 
+	                $json['data']= "false send post";
+	            }else{
+	            	//print_r($result1);
+	            	$return_data=json_decode($result1);
+	            	if ($return_data->flag!="OK") {
+	            		$json['data']= $return_data->flag;
+	            	}else{
+						$filename=$return_data->new_filename;
+						$data_so['file']=$filename;					
+					}
+	        }
+        }
+        
+        $this->m_campaign->update_campaign($data_so,$campaign_id);
+        echo json_encode($json);
 		
 	}
 	public function get_propos_detail()
@@ -99,6 +172,7 @@ class Campaign extends CI_Controller {
         $this->m_campaign->update_campaign_has_creator($data_2,$_POST['id']);
         $data_camp = array(
         		'status' => "WIP",
+        		'accept_budget' => $propos->propos_cost,
         	);
         $this->m_campaign->update_campaign($data_camp,$propos->campaign_id);
         $json['data']="รับข้อเสนอ เรียบร้อยแล้ว";
--- application/views/brand/v_campaign_detail_wip.php
+++ application/views/brand/v_campaign_detail_wip.php
@@ -9,43 +9,84 @@ $ci =&get_instance();
             <div class="row">
                 <div class="medium-6 columns">
                     <label>ชื่องาน
-                        <input name="name" type="text" placeholder="name" value="<?=$campaign->name?>">
+                        <input id="name" type="text" placeholder="name" value="<?=$campaign->name?>">
                     </label>
                 </div>
                 <fieldset class="large-6 columns">
                     <legend>ประเภท social</legend>
-                    <input id="checkbox1" type="checkbox" name="social[]" value="facebook" <?php if (isset($campaign->social["facebook"])){ echo "checked";} ?> >
-                    <label for="checkbox1">Facebook</label>
-                    <input id="checkbox2" type="checkbox" name="social[]" value="youtube" <?php if (isset($campaign->social["youtube"])){ echo "checked";} ?>>
-                    <label for="checkbox2">Youtube</label>
+                    <?php if (isset($campaign->social["facebook"])){ ?><label for="checkbox1">Facebook</label><?} ?> 
+                    <?php if (isset($campaign->social["youtube"])){ ?><label for="checkbox2">Youtube</label><?} ?>
+                    
                 </fieldset>
                 <div class="medium-6 columns">
-                    <label>ช่วงบัดเจต
-                        <input name="budget_start" type="text" placeholder="budget-start" value="<?=$campaign->budget_start?>">-<input name="budget_end" type="text" placeholder="budget-end" value="<?=$campaign->budget_end?>">
+                    <label>ค่าใช้จ่าย : <?=number_format($campaign->accept_budget)?>                        
                     </label>
                 </div>
                 <div class="medium-12 columns">
                     <label>รายละเอียด
-                        <textarea name="description" placeholder="None" rows="10"><?=$campaign->description?></textarea>
+                        <textarea id="description" placeholder="None" rows="10"><?=$campaign->description?></textarea>
                     </label>
                 </div>
                 <div class="medium-4 columns">
                     <label>url ร้าน
-                        <input name="url" type="text" placeholder="http://exp.com" value="<?=$campaign->url?>">
+                        <input id="url" type="text" placeholder="http://exp.com" value="<?=$campaign->url?>">
                     </label>
                 </div>
                 <div class="medium-4 columns">
                     <label>วันที่เริ่ม
-                        <input type="text" name="start_date" class="datepicker" value="<?=$ci->m_time->unix_to_datepicker($campaign->start_date)?>">
+                        <input type="text" id="start_date" class="datepicker" value="<?=$ci->m_time->unix_to_datepicker($campaign->start_date)?>">
                     </label>
                 </div>
                 <div class="medium-4 columns">
                     <label>วันที่สิ้นสุด
-                        <input type="text" name="end_date" class="datepicker" value="<?=$ci->m_time->unix_to_datepicker($campaign->start_date)?>">
+                        <input type="text" id="end_date" class="datepicker" value="<?=$ci->m_time->unix_to_datepicker($campaign->start_date)?>">
                     </label>
                 </div>
                 <div class="medium-5 columns">
-                    <a href="javascript:;" class="button secondary">Update</a>
+                    <a href="javascript:update_camp('<?=$campaign->id?>');" class="button secondary">Update</a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="medium-12 columns">
+                    <label>Update งาน
+                        <textarea id="progress" placeholder="None" rows="10"><?=$campaign->progress?></textarea>
+                    </label>
+                </div>
+                <div class="medium-12 columns">
+                    <label>ส่งไฟล์งาน
+                        
+                    </label>
+                </div>
+                <div class="large-3 columns">
+                            <!-- The fileinput-button span is used to style the file input field as button -->
+                            <span class="button success fileinput-button">
+                            <span>Select files...</span>
+                            <!-- The file input field used as target for the file upload widget -->
+                            <input id="fileupload" type="file" name="files[]" multiple>
+                            </span>
+                            <br>
+                            <br>
+                            <!-- The global progress bar -->
+                            <div id="progress" class="success progress">
+                                <div class="progress-meter"></div>
+                            </div>
+                            <!-- The container for the uploaded files -->
+                            <div class="files">
+                                <img id="img_tmp" src="" alt="">
+                                <input id="files" type="hidden" name="filename" value="">
+                            </div>
+                        </div>
+                <div class="medium-5 columns">
+                    <a href="javascript:update_camp_creator('<?=$campaign->id?>');" class="button secondary">Update</a>
+                    <?
+                    if ($campaign->file!="") {
+                        ?>
+                        <a href="<?echo upload_site_url('media/campaign/submit/'.$campaign->file);?>" class="button secondary">Download file</a>
+                        <?
+                    }
+                    ?>
+                    
                 </div>
             </div>
         </div>
@@ -70,6 +111,46 @@ $ci =&get_instance();
     });
     
   }
+  function update_camp(camp_id) {
+
+        $.ajax({
+                method: "post",
+                url: "<?php echo site_url("ajax/campaign/update_camp_detail"); ?>",
+                data: {
+                    'camp_id':camp_id,
+                    'name':$("#name").val(),
+                    'description':$("#description").val(),
+                    'url':$("#url").val(),
+                    'start_date':$("#start_date").val(),
+                    'end_date':$("#end_date").val(),
+                }
+            })
+            .done(function(data) {
+                    $("#detail-modal .camp-region").html("บันทึกเรียบร้อยแล้ว");
+                    $("#detail-modal").foundation('open');;
+                    
+            });
+
+    }
+    function update_camp_creator(camp_id) {
+
+        $.ajax({
+                method: "post",
+                url: "<?php echo site_url("ajax/campaign/update_camp_detail_cre"); ?>",
+                data: {
+                    'camp_id':camp_id,
+                    'progress':$("#progress").val(),
+                    'file':$("#files").val(),
+                }
+            })
+            .done(function(data) {
+                    $("#detail-modal .camp-region").html("บันทึกเรียบร้อยแล้ว");
+                    $("#detail-modal").foundation('open');
+                    $("#files").val("");
+                    
+            });
+
+    }
     $(function() {
 
         $( ".datepicker" ).datepicker({
@@ -79,3 +160,41 @@ $ci =&get_instance();
           });
     });
 </script>
+<!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
+    <script src="<?=site_url()?>/js/vendor/jquery.ui.widget.js"></script>
+    <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+    <script src="<?=site_url()?>/js/jquery.iframe-transport.js"></script>
+    <!-- The basic File Upload plugin -->
+    <script src="<?=site_url()?>/js/jquery.fileupload.js"></script>
+    <script type="text/javascript">
+    $(function() {
+        'use strict';
+        // Change this to the location of your server-side upload handler:
+        var url = '<?php echo upload_site_url('upload/fileupload');?>';
+        $('#fileupload').fileupload({
+                url: url,
+                dataType: 'json',
+                done: function(e, data) {
+                    //console.log(data);
+                    $.each(data.result.files, function(index, file) {
+                        //console.log(file);
+                        if (file.error == "File is too big") {
+                            $("#img_tmp").attr('alt', 'File is too big');
+                            $("#files").attr('value', '');
+                        } else {
+                            $("#img_tmp").attr('src', '<?echo upload_site_url();?>media/temp/' + file.name);
+                            $("#files").val(file.name);
+                        }
+                    });
+                },
+                progressall: function(e, data) {
+                    var progress = parseInt(data.loaded / data.total * 100, 10);
+                    $('#progress .progress-meter').css(
+                        'width',
+                        progress + '%'
+                    );
+                }
+            }).prop('disabled', !$.support.fileInput)
+            .parent().addClass($.support.fileInput ? undefined : 'disabled');
+    });
+    </script>
